e0de92546e2f10693b3ef72d012346a63d3a835b
kishor yd <114437235+kishoryd@users.noreply.github.com>
2023-03-07 10:33:45 +0000

    added connection to database

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 
@@ -28,9 +30,23 @@ JOBS = [
   },
 ]
 
+def load_jobs_from_db():
+  with engine.connect() as conn:
+  result = conn.execute(text("select * from jobs"))
+
+  jobs = []
+  for row in result.all():
+    # get the column names
+    keys = result.keys()
+    # create a dictionary for the row
+    row_dict = dict(zip(keys, row))
+    # append the dictionary to the list
+    jobs.append(row_dict)
+  return jobs  
 
 @app.route("/")
 def hello_company():
+  #jobs = load_jobs_from_db()
   return render_template('home.html', jobs=JOBS)
 
 

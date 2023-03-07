ed4f6702bdcdfef265a70ccfdd8182cf3798de6b
kishor yd <114437235+kishoryd@users.noreply.github.com>
2023-03-07 10:45:19 +0000

    made some changes

--- app.py
+++ app.py
@@ -32,17 +32,17 @@ JOBS = [
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
+   result = conn.execute(text("select * from jobs"))
 
-  jobs = []
-  for row in result.all():
-    # get the column names
-    keys = result.keys()
-    # create a dictionary for the row
-    row_dict = dict(zip(keys, row))
-    # append the dictionary to the list
-    jobs.append(row_dict)
-  return jobs  
+   jobs = []
+   for row in result.all():
+     # get the column names
+     keys = result.keys()
+     # create a dictionary for the row
+     row_dict = dict(zip(keys, row))
+     # append the dictionary to the list
+     jobs.append(row_dict)
+   return jobs 
 
 @app.route("/")
 def hello_company():

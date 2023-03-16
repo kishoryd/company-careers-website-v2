666831490c22967e4768e907907e4711bbffa0e1
kishor yd <114437235+kishoryd@users.noreply.github.com>
2023-03-16 17:19:45 +0000

    made some changes

--- database.py
+++ database.py
@@ -3,37 +3,32 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string, 
-  connect_args= {
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  } )
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-   result = conn.execute(text("select * from jobs"))
+    result = conn.execute(text("select * from jobs"))
 
-   jobs = []
-   for row in result.all():
-     # get the column names
-     keys = result.keys()
-     # create a dictionary for the row
-     row_dict = dict(zip(keys, row))
-     # append the dictionary to the list
-     jobs.append(row_dict)
-   return jobs
-    
+    jobs = []
+    for row in result.all():
+      # get the column names
+      keys = result.keys()
+      # create a dictionary for the row
+      row_dict = dict(zip(keys, row))
+      # append the dictionary to the list
+      jobs.append(row_dict)
+    return jobs
+
+
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-      text("SELECT * FROM jobs WHERE id = :val"),
-      val=id
-    )
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), val=id)
     rows = result.all()
     if len(rows) == 0:
       return None
     else:
       return dict(rows[0])
-  
